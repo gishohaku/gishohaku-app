01696b2450f85d05ac5b8084aa130649dd7195ee
mottox2 <y.mottox2@gmail.com>
2019-05-02 17:08:59 +0900

    Add saveUser function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,9 @@
-const onRequest = require('firebase-functions').https.onRequest
+const admin = require('firebase-admin')
+const functions = require('firebase-functions')
+
+const onRequest = functions.https.onRequest
+
+admin.initializeApp();
 
 // const index = require('./next/serverless/pages/index')
 // const books = require('./next/serverless/pages/books')
@@ -16,3 +21,15 @@ const handle = app.getRequestHandler()
 exports.app = onRequest((req, res) => (app.prepare().then(
   () => handle(req, res)
 )))
+
+exports.saveUser = functions.auth.user().onCreate((user) => {
+  console.log(user)
+  userDoc = {
+    email: user.email,
+    displayName: user.displayName
+  }
+  admin.firestore().collection('users').doc(user.uid).set(userDoc).then(result => {
+    console.log(result)
+    return
+  })
+})
