039452ec6ff5a9f0ff24ad8c1e6c964569131338
mottox2 <y.mottox2@gmail.com>
2019-05-12 23:50:11 +0900

    Add BookCell

---
+++ app/components/BookCell.tsx
@@ -0,0 +1,137 @@
+/** @jsx jsx */
+import Link from 'next/link'
+
+import { jsx, css, Global } from '@emotion/core'
+import marked from 'marked'
+
+import Book, { types, mediums } from '../utils/book'
+import ImageBox from '../components/ImageBox';
+
+const Label: React.FC<{
+  text: string
+  color?: string
+  backgroundColor?: string
+}> = ({ text, color, backgroundColor }) => {
+
+  return <p css={css`
+    font-size: 12px ;
+    padding: 4px 8px;
+    line-height: 1.2;
+    display: inline-block;
+    background-color: ${backgroundColor || '#e1e5ec'};
+    border-radius: 2px;
+    margin-right: 4px;
+    color: ${color || 'rgba(0, 0, 0, 0.6)'};
+    font-weight: bold;
+  `}>{text}</p>
+}
+
+interface Props {
+  book: Book
+  editable: boolean
+}
+
+const BookCell: React.SFC<Props> = ({ book, editable = false }) => {
+
+  const metadata = [
+    book.type && `${types[book.type]}`,
+    book.pages > 0 && `${book.pages}ページ`,
+    book.stock > 0 && `${book.stock}部頒布予定`,
+    book.medium && `${mediums[book.medium]}`,
+  ].filter(el => el)
+  return <div css={css`
+      background-color: white;
+      margin-bottom: 20px;
+      padding: 20px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.08);
+      border-radius: 8px;
+    `} key={book.id}>
+    <div css={css`
+        display: flex;
+        align-items: center;
+        margin-bottom: 20px;
+      `}>
+      <div>
+        <div css={css`
+            font-size: 20px;
+            font-weight: bold;
+          `}>
+          {book.title}
+        </div>
+        <div css={css`
+            font-size: 13px;
+          `}>
+          {book.isNew && <Label backgroundColor={'#ECB40D'} color={'white'} text='新刊' />}
+          <span css={css`
+              opacity: 0.6;
+            `}>
+            {metadata.join("・")}
+          </span>
+        </div>
+      </div>
+      {editable &&
+        <Link href={`/books/edit?id=${book.id}`} as={`/books/${book.id}/edit`} passHref>
+          <a css={css`
+              margin-left: auto;
+              border: 1px solid #2A5773;
+              text-decoration: none;
+              padding: 6px 20px;
+              border-radius: 4px;
+              font-size: 15px;
+              font-weight: 600;
+              color: #2A5773;
+              transition: all .2s ease;
+              white-space: nowrap;
+              &:hover {
+                background-color: #2A5773;
+                color: white;
+              }
+            `}>
+            編集
+            </a>
+        </Link>
+      }
+    </div>
+    <div css={css`
+        overflow-x: auto;
+        margin: 0 -20px;
+        padding: 0 20px;
+      `}>
+      <div css={css`
+          display: flex;
+        `}>
+        {
+          book.images.map((image, index) => {
+            return <ImageBox imageUrl={image} size='square' key={index} />
+          })
+        }
+      </div>
+    </div>
+    <div css={css`
+        opacity: 0.8;
+        margin-top: 12px;
+        color: #444;
+
+        p, ul, ol {
+          margin-bottom: 12px;
+          &:last-child {
+            margin-bottom: 0;
+          }
+        }
+
+        h1, h2, h3, h4, h5, h6, strong {
+          font-weight: bold;
+          color: #222;
+        }
+      `}
+      dangerouslySetInnerHTML={{
+        __html: marked(book.description, {
+          gfm: true,
+          breaks: true,
+          sanitize: true
+        })
+      }} />
+  </div>
+}
+
+export default BookCell
--- app/pages/mypage.tsx
+++ app/pages/mypage.tsx
@@ -6,7 +6,6 @@ import 'firebase/firestore'
 import 'firebase/functions'
 
 import { jsx, css, Global } from '@emotion/core'
-import marked from 'marked'
 
 import circleTumbnail from '../images/circle.png'
 import editIcon from '../images/edit.svg'
@@ -19,6 +18,7 @@ import Book, { types, mediums } from '../utils/book'
 import { useContext, useEffect, useState } from 'react'
 import UserContext from '../contexts/UserContext';
 import MessageBox from '../components/MessageBox';
+import BookCell from '../components/BookCell';
 import Loader from '../components/Loader';
 import { media } from '../utils/style'
 import ImageBox from '../components/ImageBox';
@@ -190,107 +190,7 @@ const Mypage = (props: any) => {
           flex: 1;
           width: 100%;
         `}>
-          {
-            books.map(book => {
-              const metadata = [
-                book.type && `${types[book.type]}`,
-                book.pages > 0 && `${book.pages}ページ`,
-                book.stock > 0 && `${book.stock}部頒布予定`,
-                book.medium && `${mediums[book.medium]}`,
-              ].filter(el => el)
-              return <div css={css`
-                background-color: white;
-                margin-bottom: 20px;
-                padding: 20px;
-                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.08);
-                border-radius: 8px;
-              `} key={book.id}>
-                <div css={css`
-                  display: flex;
-                  align-items: center;
-                  margin-bottom: 20px;
-                `}>
-                  <div>
-                    <div css={css`
-                      font-size: 20px;
-                      font-weight: bold;
-                    `}>
-                      {book.title}
-                    </div>
-                    <div css={css`
-                      font-size: 13px;
-                    `}>
-                      {book.isNew && <Label backgroundColor={'#ECB40D'} color={'white'} text='新刊' />}
-                      <span css={css`
-                        opacity: 0.6;
-                      `}>
-                        {metadata.join("・")}
-                      </span>
-                    </div>
-                  </div>
-                  <Link href={`/books/edit?id=${book.id}`} as={`/books/${book.id}/edit`} passHref>
-                    <a css={css`
-                      margin-left: auto;
-                      border: 1px solid #2A5773;
-                      text-decoration: none;
-                      padding: 6px 20px;
-                      border-radius: 4px;
-                      font-size: 15px;
-                      font-weight: 600;
-                      color: #2A5773;
-                      transition: all .2s ease;
-                      white-space: nowrap;
-                      &:hover {
-                        background-color: #2A5773;
-                        color: white;
-                      }
-                    `}>
-                      編集
-                    </a>
-                  </Link>
-                </div>
-                <div css={css`
-                  overflow-x: auto;
-                  margin: 0 -20px;
-                  padding: 0 20px;
-                `}>
-                  <div css={css`
-                    display: flex;
-                  `}>
-                    {
-                      book.images.map((image, index) => {
-                        return <ImageBox imageUrl={image} size='square' key={index} />
-                      })
-                    }
-                  </div>
-                </div>
-                <div css={css`
-                  opacity: 0.8;
-                  margin-top: 12px;
-                  color: #444;
-
-                  p, ul, ol {
-                    margin-bottom: 12px;
-                    &:last-child {
-                      margin-bottom: 0;
-                    }
-                  }
-
-                  h1, h2, h3, h4, h5, h6, strong {
-                    font-weight: bold;
-                    color: #222;
-                  }
-                `}
-                  dangerouslySetInnerHTML={{
-                    __html: marked(book.description, {
-                      gfm: true,
-                      breaks: true,
-                      sanitize: true
-                    })
-                  }} />
-              </div>
-            })
-          }
+          {books.map(book => <BookCell book={book} editable={true} />)}
           <Link href='/books/new'>
             <a css={css`
               display: block;
