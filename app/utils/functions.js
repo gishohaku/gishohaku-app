2ce0a44a694db0e181448a48756d97e35119e630
mottox2 <y.mottox2@gmail.com>
2019-05-17 16:46:12 +0900

    Change function endpoint

--- app/utils/functions.js
+++ app/utils/functions.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 export const getBooks = async () => {
-  const result = await axios.get('https://us-central1-next-serverless-app.cloudfunctions.net/apiBooks')
+  const result = await axios.get('https://us-central1-gishohaku.cloudfunctions.net/apiBooks')
   return result.data
 }
 
 export const getCircles = async () => {
-  const result = await axios.get('https://us-central1-next-serverless-app.cloudfunctions.net/apiCircles')
+  const result = await axios.get('https://us-central1-gishohaku.cloudfunctions.net/apiCircles')
   return result.data
 }
