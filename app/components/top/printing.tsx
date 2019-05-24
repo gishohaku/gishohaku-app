d34ea9e7dfc6c6b4bac43bd8f3f12aea9b164c43
mottox2 <y.mottox2@gmail.com>
2019-05-25 00:36:47 +0900

    Fix

---
+++ app/components/top/printing.tsx
@@ -0,0 +1,48 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core"
+import { Embed } from 'sancho'
+import defaultImage from '../../images/sponsor.png'
+import LazyLoad from 'react-lazyload'
+import { colors } from '../../utils/style'
+
+interface Props {
+  image?: string
+  name: string
+  url: string
+}
+
+const printing : React.FC<Props> = ({image, name, url}) => {
+  return <a href={url} target='_blank' rel='noopner'>
+    <Embed width={200} height={40} css={css`
+      border: 1px solid #eee;
+      background-color: white;
+      display: inline-block;
+      width: 200px;
+      height: 40px;
+      margin: 16px 16px;
+      text-align: center;
+      vertical-align: middle;
+      p {
+        line-height: 40px;
+        font-weight: 600;
+        color: inherit;
+      }
+      &:hover {
+        border-color: ${colors.primary};
+      }
+    `}>
+      { image ?
+        <LazyLoad>
+          <img src={image} css={css`
+            object-fit: contain;
+            display: block;
+          `} />
+        </LazyLoad> : <p>
+          {name}
+        </p>
+      }
+    </Embed>
+  </a>
+}
+
+export default printing
