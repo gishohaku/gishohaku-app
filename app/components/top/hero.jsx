7c6cbdd1170f47f1ff4b173338bbb948860a754a
Akira Morikawa <akira_morikawa@mediado.jp>
2019-05-31 10:17:30 +0900

    スタッフ募集リンク追加

--- app/components/top/hero.jsx
+++ app/components/top/hero.jsx
@@ -156,6 +156,34 @@ const Hero = () => {
         サークルリスト
         </a>
       </Link>
+      <Link href="/staffs" passHref>
+        <a
+          css={css`
+            background-color: ${colors.accent};
+            font-size: 16px;
+            font-weight: bold;
+            color: white;
+            padding: 11px 36px 13px;
+            min-width: 270px;
+            display: inline-block;
+            border-radius: 30px;
+            margin-top: 24px;
+            text-decoration: none;
+            transition: transform 0.15s ease-out;
+            transform: translateY(0);
+            &:hover {
+              transform: translateY(-1px);
+              background-color: #dbae29;
+            }
+            // background-color: #aaaaaa;
+            // &:hover {
+            //   background-color: #aaaaaa;
+            // }
+          `}
+        >
+          当日スタッフ募集中！
+        </a>
+      </Link>
     </section>
   )
 }
