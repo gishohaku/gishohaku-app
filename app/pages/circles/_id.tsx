7220f8fb257654e6e7cbbe4823f71c49007e0f91
mottox2 <y.mottox2@gmail.com>
2019-06-04 02:46:16 +0900

    Sort

--- app/pages/circles/_id.tsx
+++ app/pages/circles/_id.tsx
@@ -12,29 +12,33 @@ import { refToPath } from '../../utils/firebase'
 import { initFirebase } from '../../utils/firebase'
 import Circle, { categories } from '../../utils/circle'
 import Book, { types, mediums } from '../../utils/book'
-import CircleDetail from '../../components/CircleDetail';
-import SEO from '../../components/SEO';
-import { NextFunctionComponent, NextContext } from 'next';
+import CircleDetail from '../../components/CircleDetail'
+import SEO from '../../components/SEO'
+import { NextFunctionComponent, NextContext } from 'next'
 
 interface Props {
   circle: Circle
   books: Book[]
 }
 
-const CirclePage: NextFunctionComponent<Props> = (props) => {
+const CirclePage: NextFunctionComponent<Props> = props => {
   // const [books, setBooks] = useState<Book[]>([])
   // const [circle, setCircle] = useState<Circle>()
   const { circle, books } = props
 
-  return <>
-    <SEO title={circle.name} />
-    <CircleDetail circle={circle} books={books} editable={false} />
-  </>
+  return (
+    <>
+      <SEO title={circle.name} />
+      <CircleDetail circle={circle} books={books} editable={false} />
+    </>
+  )
 }
 
-CirclePage.getInitialProps = async (context: NextContext<{
-  id: string
-}>) => {
+CirclePage.getInitialProps = async (
+  context: NextContext<{
+    id: string
+  }>
+) => {
   initFirebase()
   const id: string = context.query.id
   if (!id) {
@@ -43,13 +47,16 @@ CirclePage.getInitialProps = async (context: NextContext<{
   const db = firebase.firestore()
   const circleRef = db.collection('circles').doc(id)
   const circle = await circleRef.get()
-  const snapshots = await db.collection('books').where("circleRef", "==", circleRef).get()
+  const snapshots = await db
+    .collection('books')
+    .where('circleRef', '==', circleRef)
+    .get()
   let books: Book[] = []
   snapshots.forEach(book => {
     const data = book.data()
     books.push({
       id: book.id,
-      ...refToPath(data, 'circleRef') as Book
+      ...(refToPath(data, 'circleRef') as Book)
     })
   })
   console.log(circle)
