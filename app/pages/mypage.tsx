2e8359386c34da5a3505e4e1ba7fa028e19a4982
mottox2 <y.mottox2@gmail.com>
2019-06-23 23:36:53 +0900

    Use sancho UI

--- app/pages/mypage.tsx
+++ app/pages/mypage.tsx
@@ -3,39 +3,39 @@ import Link from 'next/link'
 
 import { jsx, css } from '@emotion/core'
 
-import { Button, Divider, Container, IconChevronRight } from 'sancho'
+import { Button, Divider, Container, IconChevronRight, List, ListItem } from 'sancho'
 import { withRouter } from 'next/router'
 import { useContext } from 'react'
 import UserContext from '../contexts/UserContext'
 import MessageBox from '../components/MessageBox'
 import Loader from '../components/Loader'
 
-const Item = ({ primary, secondary, ...other }) => {
-  return (
-    <a
-      css={css`
-        padding: 12px;
-        display: block;
-        text-decoration: none;
-        color: inherit;
-        &:hover {
-          background-color: #eee;
-        }
-      `}
-      {...other}
-    >
-      {primary}
-      <div
-        css={css`
-          font-size: 12px;
-          opacity: 0.6;
-        `}
-      >
-        {secondary}
-      </div>
-    </a>
-  )
-}
+// const Item = ({ primary, secondary, ...other }) => {
+//   return (
+//     <a
+//       css={css`
+//         padding: 12px;
+//         display: block;
+//         text-decoration: none;
+//         color: inherit;
+//         &:hover {
+//           background-color: #eee;
+//         }
+//       `}
+//       {...other}
+//     >
+//       {primary}
+//       <div
+//         css={css`
+//           font-size: 12px;
+//           opacity: 0.6;
+//         `}
+//       >
+//         {secondary}
+//       </div>
+//     </a>
+//   )
+// }
 
 const Mypage: React.FC = () => {
   const { user, isUserLoading, userData } = useContext(UserContext)
@@ -76,34 +76,34 @@ const Mypage: React.FC = () => {
           box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12);
         `}
       >
-        {/* <List> */}
-        <Link href="/circles?starred" passHref>
-          <Item
-            primary="チェックしたサークル"
-            secondary="チェックをつけたサークルを確認できます"
-            contentAfter={<IconChevronRight />}
-          />
-        </Link>
-        <Link href="/books?starred" passHref>
-          <Item
-            primary="チェックした頒布物"
-            secondary="チェックをつけた頒布物が確認できます"
-            contentAfter={<IconChevronRight />}
-          />
-        </Link>
-        {userData && userData.circleRef && (
-          <>
-            <Divider />
-            <Link href="/mypage/circle" passHref>
-              <Item
-                primary="サークル情報編集"
-                secondary="サークル情報の編集、頒布物の登録、チェック数の確認を行えます"
-                contentAfter={<IconChevronRight />}
-              />
-            </Link>
-          </>
-        )}
-        {/* </List> */}
+        <List>
+          <Link href="/circles?starred" passHref>
+            <ListItem
+              primary="チェックしたサークル"
+              secondary="チェックをつけたサークルを確認できます"
+              contentAfter={<IconChevronRight />}
+            />
+          </Link>
+          <Link href="/books?starred" passHref>
+            <ListItem
+              primary="チェックした頒布物"
+              secondary="チェックをつけた頒布物が確認できます"
+              contentAfter={<IconChevronRight />}
+            />
+          </Link>
+          {userData && userData.circleRef && (
+            <>
+              <Divider />
+              <Link href="/mypage/circle" passHref>
+                <ListItem
+                  primary="サークル情報編集"
+                  secondary="サークル情報の編集、頒布物の登録、チェック数の確認を行えます"
+                  contentAfter={<IconChevronRight />}
+                />
+              </Link>
+            </>
+          )}
+        </List>
       </div>
     </Container>
   )
