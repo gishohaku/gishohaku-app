9efe0936ab1f44377c745b0d7515ea8a34d9c8dd
mottox2 <y.mottox2@gmail.com>
2019-07-26 23:16:21 +0900

    Update script

--- scripts/checkSubmissions.js
+++ scripts/checkSubmissions.js
@@ -1,9 +1,4 @@
-// const firebase = require('firebase/app')
-// import firebase from 'firebase/app'
-// import 'firebase/storage'
-// const admin = require('firebase-admin')
-import admin from 'firebase-admin'
-// require('firebase/storage')
+const admin = require('firebase-admin')
 
 admin.initializeApp({
   apiKey: process.env.API_KEY,
@@ -13,21 +8,43 @@ admin.initializeApp({
   storageBucket: process.env.STORAGE_BUCKET
 })
 
+const types = {
+  fanzine: '同人誌',
+  commerce: '商業誌'
+}
+
 const main = async () => {
-  const books = await admin.firestore().collection('books').get()
-  let bookMap = {}
-  books.forEach(snap => {
-    bookMap[snap.id] = snap.data().title
-  })
-
-  console.log(bookMap)
-
+  let submissionMap = {}
   const snaps = await admin.firestore().collection('bookSubmissions').get()
-  snaps.forEach(snap => {
-    const data = snap.data()
-    const createdAt = new Date(data.createdAt._seconds * 1000)
-    console.log([snap.id, bookMap[snap.id], data.originalName, createdAt, data.url].join(", "))
+  snaps.docs.forEach(d => {
+    submissionMap[d.id] = d.data()
   })
+
+  let circleMap = {}
+  const circleSnaps = await admin.firestore().collection('circles').get()
+  circleSnaps.docs.forEach(d => {
+    circleMap[d.id] = d.data()
+  })
+
+  const bookSnapshots = await admin.firestore().collection('books').get()
+  let bookMap = {}
+  const books = bookSnapshots.docs.map(doc => {
+    const data = doc.data()
+    const circle = circleMap[data.circleRef.path.replace('circles/', '')]
+    // console.log(data.circleRef.path, circle)
+    return {
+      id: doc.id,
+      circleNumber: circle.boothNumber,
+      ...data,
+    }
+  }).sort((book1, book2) => {
+    return book1.circleNumber - book2.circleNumber
+  })
+  // console.log(books.map(d => d.circleNumber))
+  for (let book of books) {
+    let submission = submissionMap[book.id]
+    console.log([book.id, book.circleBooth, book.circleName, types[book.type], book.title, submission && submission.url, submission && (new Date(submission.createdAt._seconds * 1000))].join(", "))
+  }
 }
 
 main()
