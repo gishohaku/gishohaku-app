326126a4c3233393fadb232073630f6808f45e4c
mottox2 <y.mottox2@gmail.com>
2019-08-12 23:59:26 +0900

    Add menu to Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,16 +5,45 @@ import logo from '../images/logo.png'
 import { media } from '../utils/style'
 
 import { jsx, css } from '@emotion/core'
+import { IconMenu, Sheet, List, ListItem, IconChevronRight } from 'sancho';
+import { useState } from 'react';
+
+const buttonSize = 48
+
+const hamburgerButton = css`
+  margin-left: auto;
+  width: ${buttonSize}px;
+  height: ${buttonSize}px;
+  background-color: #eee;
+  border-radius: ${buttonSize / 2}px;
+  position: relative;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e5e5e5;
+  }
+
+  &:active {
+    background-color: #ddd;
+  }
+
+  svg {
+    position: absolute;
+    transform: translateX(-50%) translateY(-50%);
+    top: 50%;
+    left: 50%;
+  }
+`
 
 const Header: React.FC<any> = () => {
+  const [isOpen, setOpen] = useState(false)
   return <header
     css={css`
       background-color: white;
       display: flex;
-      flex-direction: column;
       align-items: center;
       font-size: 12px;
-      padding: 0 8px;
+      padding: 0 16px;
       box-shadow: 0 2px 8px rgba(0, 0, 0, 0.08);
       min-height: 80px;
       @media ${media.small} {
@@ -22,6 +51,9 @@ const Header: React.FC<any> = () => {
       }
     `}
   >
+    <div css={css(hamburgerButton, `
+      visibility: hidden;
+    `)} />
     <Link href="/" passHref>
       <a
         css={css`
@@ -44,6 +76,19 @@ const Header: React.FC<any> = () => {
         />
       </a>
     </Link>
+    <div css={hamburgerButton} onClick={() => setOpen(true)}>
+      <IconMenu />
+    </div>
+    <Sheet position="right" onRequestClose={() => setOpen(false)} isOpen={isOpen}>
+      <List>
+        <Link href="/" passHref>
+          <ListItem primary="第1回 技術書同人誌博覧会" secondary="2019年7月27日 開催" contentAfter={<IconChevronRight />} component="a" onClick={() => setOpen(false)} />
+        </Link>
+        <Link href="/gishohaku2" passHref>
+          <ListItem primary="第2回 技術書同人誌博覧会" secondary="2019年12月14日 開催予定" contentAfter={<IconChevronRight />} component="a" onClick={() => setOpen(false)} />
+        </Link>
+      </List>
+    </Sheet>
   </header>
 }
 
--- app/components/top/index.tsx
+++ app/components/top/index.tsx
@@ -1,50 +1,10 @@
 /** @jsx jsx */
 import Hero from "./hero"
-import { css, jsx } from '@emotion/core'
+import { jsx } from '@emotion/core'
 import Sections from "./sections"
-import { media, colors } from "../../utils/style";
-import Link from "next/link";
 
 const Top = () => (
   <>
-    <Link href="/gishohaku2" passHref>
-      <a css={css`
-        background-color: ${colors.accent};
-        color: white;
-        font-weight: 600;
-        font-size: 16px;
-        padding: 10px 16px;
-        display: flex;
-        align-items: center;
-        text-decoration: none;
-        @media ${media.small} {
-          flex-direction: column;
-          align-items: start;
-        }
-        &:hover {
-          background-color: #dbae29;
-        }
-
-      `}>
-        第2回 技術書同人誌博覧会 開催決定
-        <span css={css`
-          // background-color: white;
-          color: #B28246;
-          display: inline-block;
-          margin-left: auto;
-          font-size: 14px;
-          padding: 8px 24px;
-          border-radius: 40px;
-          border: 1px solid white;
-          color: white;
-          @media ${media.small} {
-            margin: 8px 0 0;
-          }
-        `}>
-          詳しくはこちら
-        </span>
-      </a>
-    </Link>
     <Hero />
     <Sections />
   </>
