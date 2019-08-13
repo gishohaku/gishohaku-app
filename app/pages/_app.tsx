0deaa19e3763129ecf6088301c5bbceffee44d7f
mottox2 <y.mottox2@gmail.com>
2019-08-13 19:32:58 +0900

    Disable sentry

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -6,21 +6,21 @@ import { UserProvider } from '../contexts/UserContext'
 import { initFirebase } from '../utils/firebase'
 import Layout from '../components/Layout'
 import ReactGA from 'react-ga'
-import * as Sentry from '@sentry/browser'
+// import * as Sentry from '@sentry/browser'
 
 import '../lightbox.css'
 
 const TRACKING_ID = 'UA-129667923-2'
 
-if (process.env.NODE_ENV === 'production') {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN
-  })
-}
+// if (process.env.NODE_ENV === 'production') {
+//   Sentry.init({
+//     dsn: process.env.SENTRY_DSN
+//   })
+// }
 
-interface ErrorInfo extends _ErrorInfo {
-  [key: string]: string
-}
+// interface ErrorInfo extends _ErrorInfo {
+//   [key: string]: string
+// }
 
 class MyApp extends App {
   componentDidMount() {
@@ -31,15 +31,15 @@ class MyApp extends App {
     })
   }
 
-  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    Sentry.withScope((scope: any) => {
-      Object.keys(errorInfo).forEach(key => {
-        scope.setExtra(key, errorInfo[key])
-      })
-      Sentry.captureException(error)
-    })
-    super.componentDidCatch(error, errorInfo)
-  }
+  // componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+  //   Sentry.withScope((scope: any) => {
+  //     Object.keys(errorInfo).forEach(key => {
+  //       scope.setExtra(key, errorInfo[key])
+  //     })
+  //     Sentry.captureException(error)
+  //   })
+  //   super.componentDidCatch(error, errorInfo)
+  // }
 
   public render() {
     const { Component, pageProps, router } = this.props as any
