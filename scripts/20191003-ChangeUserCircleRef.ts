a8cb056a0ffaff07faf1bb90f879bd8fdc2f1d5f
mottox2 <y.mottox2@gmail.com>
2019-10-03 12:53:31 +0900

    Add script to change schema

---
+++ scripts/20191003-ChangeUserCircleRef.ts
@@ -0,0 +1,39 @@
+import admin from 'firebase-admin'
+
+admin.initializeApp({
+  projectId: process.env.PROJECT_ID,
+  databaseURL: process.env.DATABASE_URL,
+  storageBucket: process.env.STORAGE_BUCKET
+});
+
+(async () => {
+  const db = admin.firestore()
+
+  const users = await db.collection('users').get()
+  users.docs.forEach(doc => {
+    const data = doc.data()
+    if (data.circleRef)
+      doc.ref.update({
+        event: {
+          gishohaku1: data.circleRef
+        }
+      })
+  })
+
+  // 初回のみ
+  if (false) {
+    const circles = await db.collection('circles').get()
+    circles.docs.forEach(doc => {
+      doc.ref.update({
+        eventId: 'gishohaku1'
+      })
+    })
+
+    const books = await db.collection('books').get()
+    books.docs.forEach(doc => {
+      doc.ref.update({
+        eventId: 'gishohaku1'
+      })
+    })
+  }
+})()
