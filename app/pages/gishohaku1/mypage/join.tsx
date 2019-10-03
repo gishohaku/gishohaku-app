431dcab03b33c92357619a5d4ec20ac6f73b17ba
mottox2 <y.mottox2@gmail.com>
2019-10-04 03:22:59 +0900

    Fix tsconfig

--- app/pages/gishohaku1/mypage/join.tsx
+++ app/pages/gishohaku1/mypage/join.tsx
@@ -3,44 +3,39 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/functions'
 
-import Link from 'next/link'
 import { jsx, css } from '@emotion/core'
 import { Button, useToast } from 'sancho'
-import { withRouter, NextRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import { useContext, useState, useEffect } from 'react'
-import UserContext from '../../../contexts/UserContext'
+import UserContext, { User } from '../../../contexts/UserContext'
 import MessageBox from '../../../components/MessageBox'
 import Loader from '../../../components/Loader'
 import qs from 'qs'
+import withUser from '../../../withUser'
 
-// FIXME: 影響範囲が大きく汚い
-export const INVITE_STORAGE_KEY = 'INVITE_STORAGE_KEY'
-
-const Join: React.FC<{
-  router: NextRouter
-}> = props => {
+const Join: React.FC = () => {
   const toast = useToast()
-  const { user, isUserLoading, userData, reloadUser } = useContext(UserContext)
+  const router = useRouter()
+  const { reloadUser } = useContext(UserContext)
   const [isProcessing, setProcessing] = useState(false)
   const [circle, setCircle] = useState()
 
   // Static Site Exportではprops.router.queryが固定されており、自前でqueryを取得する必要がある
   // https://github.com/zeit/next.js/issues/4804
-  const { circleId, token } = qs.parse(props.router.asPath.split('?')[1])
+  const { circleId, token } = qs.parse(router.asPath.split('?')[1])
   console.log(circleId, token)
 
   useEffect(() => {
     const db: firebase.firestore.Firestore = firebase.firestore()
+    if (!circleId || !token) { return }
     const circleRef = db.collection('circles').doc(circleId)
-    circleRef.get().then(circleSnapshot => {
-      setCircle(circleSnapshot.data())
+    circleRef.get().then(snapshot => {
+      setCircle(snapshot.data())
     })
   }, [circleId])
 
   const handleClick = async () => {
-    if (isProcessing) {
-      return
-    }
+    if (isProcessing) { return }
     setProcessing(true)
     const receiveInvitation = firebase.functions().httpsCallable('receiveInvitation')
     const result = await receiveInvitation({ circleId, token })
@@ -51,53 +46,15 @@ const Join: React.FC<{
       title: 'サークルに参加しました',
       intent: 'success'
     })
-    props.router.push('/gishohaku1/mypage/circle')
+    router.push('/gishohaku1/mypage/circle')
   }
 
   if (!circleId || !token) {
     return <MessageBox title="無効なURLです。" description="URLを確認してください。" />
   }
 
-  if (isUserLoading || (user && !userData) || !circle) {
+  if (!circle) {
     return <Loader label="loading..." />
-  }
-
-  if (!user) {
-    return (
-      <MessageBox
-        title="サークルに参加する"
-        description={`「${circle.name}」に参加できます。サークルに参加するにはログインが必要です。`}
-      >
-        <Link href="/sign_in" passHref>
-          <Button
-            component="a"
-            block
-            css={css`
-              margin-top: 12px;
-            `}
-            onClick={() => {
-              localStorage.setItem(INVITE_STORAGE_KEY, props.router.asPath)
-            }}
-          >
-            ログイン
-          </Button>
-        </Link>
-        <Link href="/sign_up" passHref>
-          <Button
-            component="a"
-            block
-            css={css`
-              margin-top: 12px;
-            `}
-            onClick={() => {
-              localStorage.setItem(INVITE_STORAGE_KEY, props.router.asPath)
-            }}
-          >
-            会員登録
-          </Button>
-        </Link>
-      </MessageBox>
-    )
   }
 
   return (
@@ -120,4 +77,4 @@ const Join: React.FC<{
   )
 }
 
-export default withRouter(Join)
+export default withUser(Join)
