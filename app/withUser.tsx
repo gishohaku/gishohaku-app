afed205a12938f8b26e4d9c1d6144049ebab6fbe
mottox2 <y.mottox2@gmail.com>
2019-10-04 02:45:44 +0900

    Show login link

--- app/withUser.tsx
+++ app/withUser.tsx
@@ -1,6 +1,11 @@
+/** @jsx jsx */
 import { useContext, ComponentType } from 'react'
 import UserContext from './contexts/UserContext'
 import Loader from './components/Loader'
+import MessageBox from './components/MessageBox'
+import Link from 'next/link'
+import { Button } from 'sancho'
+import { jsx, css } from '@emotion/core'
 
 const withUser = (WrappedComponent: ComponentType<any>) => (props: any) => {
   const { isUserLoading, user, userData } = useContext(UserContext)
@@ -10,7 +15,23 @@ const withUser = (WrappedComponent: ComponentType<any>) => (props: any) => {
   }
 
   if (!userData) {
-    return <p>ログインが必要です</p>
+    return <MessageBox
+      title="ログインが必要です。"
+      description="このページを利用するにはログインが必要です。"
+    >
+      <Link href="/sign_in" passHref>
+        <Button
+          component="a"
+          css={css`
+          margin-top: 12px;
+          width: 100%;
+        `}
+        >
+          ログイン
+      </Button>
+      </Link>
+    </MessageBox>
+
   }
 
   return <WrappedComponent user={user} userData={userData} {...props} />
