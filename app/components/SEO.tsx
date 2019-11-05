b706c6d2aa2a84978aca153609a7fd2bb3bbdffa
Akira Morikawa <akira_morikawa@mediado.jp>
2019-11-05 15:39:54 +0900

    fix json-ld tag

--- app/components/SEO.tsx
+++ app/components/SEO.tsx
@@ -7,6 +7,28 @@ interface Props {
   imageUrl?: string
 }
 
+const jsonLdTags = {
+  "@context": "http://schema.org",
+  "@type": "Event",
+  "name": "第二回 技術書同人誌博覧会",
+  "description": "あなたが最も得意とする技術、最も世界中に広めたい技術を、『技術書』という形で共有しませんか？ 技書博は、ITエンジニアが知見を共有するために開催される、技術書オンリーイベントです。",
+  "startDate": "2019-12-14",
+  "endDate": "2019-12-14",
+  "image": "https://gishohaku.dev/static/ogp.jpg",
+  "url": "https://gishohaku.dev/",
+  "location" : {
+    "@type" : "Place",
+    "name" : "プラザマーム",
+    "address": {
+      @type": "PostalAddress",
+      "addressLocality": "東京都",
+      "addressRegion": "JP",
+      "postalCode": "103-0007",
+      "streetAddress": "東京都中央区日本橋浜町1丁目1-12（プラザANSビル2F,3F）"
+    }
+  }
+};
+
 const SEO: React.FC<Props> = ({ description, title, imageUrl }) => {
   const metaTitle = [title, '第二回 技術書同人誌博覧会'].filter(o => o).join(' | ')
   const metaDescription = [
@@ -28,29 +50,7 @@ const SEO: React.FC<Props> = ({ description, title, imageUrl }) => {
       <meta name="twitter:description" content={metaDescription} key="twitter:description" />
       <meta name="description" content={metaDescription} key="description" />
       <meta name="keywords" content="同人誌即売会, 技術同人誌, 技書博" key="keywords" />
-      <script type="application/ld+json">
-      \\{
-        "@context": "http://schema.org",
-        "@type": "Event",
-        "name": {metaTitle},
-        "description": {metaDescription},
-        "startDate": "2019-12-14",
-        "endDate": "2019-12-14",
-        "image": "https://gishohaku.dev/static/ogp.jpg",
-        "url": "https://gishohaku.dev/",
-        "location" : \\{
-          "@type" : "Place",
-          "name" : "プラザマーム",
-          "address": \\{
-            @type": "PostalAddress",
-            "addressLocality": "東京都",
-            "addressRegion": "JP",
-            "postalCode": "103-0007",
-            "streetAddress": "東京都中央区日本橋浜町1丁目1-12（プラザANSビル2F,3F）"
-          \\}
-        \\}
-      \\}
-      </script>
+      <script type="application/ld+json">{JSON.stringify(jsonLdTags)}</script>
     </Head>
   )
 }
