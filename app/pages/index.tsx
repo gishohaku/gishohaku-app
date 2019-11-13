14eef884ca2fe6e502e0460666b186b858e50c15
mottox2 <y.mottox2@gmail.com>
2019-11-13 23:09:21 +0900

    Update home

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,7 +2,10 @@
 import Sections from "../components/top/sections"
 import { css, jsx } from '@emotion/core'
 import logo from '../images/fullLogo.png'
-import { media, colors } from '../utils/style'
+import { media } from '../utils/style'
+import TextBlock from "../components/atoms/TextBlock"
+import placeIcon from '../components/top/round-place.svg'
+import directionsIcon from '../components/top/round-directions_run.svg'
 
 const orange = '#E1740C'
 
@@ -38,7 +41,7 @@ const Hero = () => (
     justify-content: center;
     @media ${media.small} {
       flex-direction: column;
-      padding: 12px;
+      padding: 16px;
       align-items: start;
     }
   `}>
@@ -49,7 +52,7 @@ const Hero = () => (
         margin-left: -8px;
 
       }
-    `} src={logo} width={200} />
+    `} src={logo} width={200} height={256} />
     <div css={css`
       margin-left: 24px;
       @media ${media.small} {
@@ -111,9 +114,128 @@ const Hero = () => (
   </div>
 )
 
+const section = css`
+  padding: 48px 0;
+  background-color: white;
+  @media ${media.small} {
+    padding: 32px 0;
+  }
+`
+
 export default () => {
   return <>
     <Hero />
-    <Sections />
+    <section css={section}>
+      <SectionHeader en="ABOUT">技書博とは？</SectionHeader>
+      <TextBlock>
+        <p>
+          技術書同人誌博覧会（技書博）は、エンジニア（おもにITエンジニア）が自身の知見を「本」という形で共有するために開催される、技術書オンリーイベント（同人誌頒布即売会）です。
+          私たちエンジニアは、日々の業務や学習によって得た膨大な知識を記憶し、活かしています。
+          その知識の大半は「暗黙知」として個人の中に留まり続け、他者に共有できていない有用な情報がたくさんあるのではないでしょうか。
+          私たちは、知識を「集合知」としてコミュニティに還元しあうことによって、何倍もの知識を吸収でき、よりよい世界を作り出せると考えています。
+        </p>
+      </TextBlock>
+    </section>
+    <section css={[section, css`background-color: #f7f8fa;`]}>
+      <SectionHeader en="ACCESS">アクセス</SectionHeader>
+      <TextBlock>
+        <p
+          css={css`
+            font-size: 32px;
+            font-weight: bold;
+            text-align: center;
+            @media ${media.small} {
+              font-size: 24px;
+            }
+          `}
+        >
+          プラザマーム
+        </p>
+        <div
+          css={css`
+            background-color: #fff;
+            padding: 12px;
+            border-radius: 4px;
+            margin-top: 12px;
+          `}
+        >
+          <p css={withIcon}>
+            <img src={placeIcon} alt="住所" />
+            東京都中央区日本橋浜町1-1-12（
+            <a href="https://goo.gl/maps/8b45soYqMvtZK2498">
+              Google マップ
+            </a>
+            ）
+          </p>
+          <p css={withIcon}>
+            <img src={directionsIcon} alt="アクセス" />
+            浅草線 人形町駅より徒歩6分・浅草線 東日本橋駅より徒歩4分
+            <br />
+            新宿線 浜町駅より徒歩2分・新宿線 馬喰横山駅より徒歩8分
+            <br />
+            日比谷線 人形町駅より徒歩6分
+            <br />
+            半蔵門線 水天宮前駅より徒歩15分
+            <br />
+            総武線 馬喰町駅より徒歩8分
+          </p>
+        </div>
+      </TextBlock>
+    </section>
+    {/* <Sections /> */}
   </>
+}
+
+const withIcon = css`
+  position: relative;
+  padding-left: 30px;
+  margin-top: 4px;
+  img {
+    position: absolute;
+    left: 0;
+    opacity: 0.3;
+  }
+`
+
+const SectionHeader: React.FC<{
+  en: string
+}> = ({ children, en }) => {
+  return <h2 css={css`
+    font-weight: bold;
+    margin: 0 16px;
+    font-size: 32px;
+    text-align: center;
+    padding-bottom: ${12 + 3}px;
+    position: relative;
+    @media ${media.small} {
+      font-size: 24px;
+      text-align: left;
+    }
+    &:after {
+      content: ' ';
+      position: absolute;
+      height: 3px;
+      width: 80px;
+      bottom: 0;
+      display: block;
+      background-color: ${orange};
+      left: 50%;
+      transform: translateX(-50%);
+      @media ${media.small} {
+        left: auto;
+        transform: none;
+      }
+    }
+    small {
+      display: block;
+      font-family: 'Poppins', sans-serif;
+      font-size: 18px;
+      font-weight: 500;
+      opacity: 0.6;
+      line-height: 1;
+      margin-bottom: 4px;
+    }
+  `}>
+    <small>{en}</small>{children}
+  </h2>
 }
