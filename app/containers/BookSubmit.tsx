6c9de8172b8550fa8de6b02fc832d4c63c77ea37
mottox2 <y.mottox2@gmail.com>
2019-11-23 07:34:14 +0900

    Refactor

--- app/containers/BookSubmit.tsx
+++ app/containers/BookSubmit.tsx
@@ -1,18 +1,15 @@
 /** @jsx jsx */
-import { useEffect, useState, useContext } from 'react'
-
+import { useContext } from 'react'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
-
 import { jsx, css } from '@emotion/core'
 
-import Book from '../../../../utils/book'
-import FormContainer from '../../../../components/FormContainer'
-import Loader from '../../../../components/Loader'
-import BookSubmitForm from '../../../../components/BookSubmitForm'
-import withUser from '../../../../withUser'
-import EventContext from '../../../../contexts/EventContext'
-import { db } from '../../../../utils/firebase'
+import FormContainer from '../components/FormContainer'
+import Loader from '../components/Loader'
+import BookSubmitForm from '../components/BookSubmitForm'
+import withUser from '../withUser'
+import EventContext from '../contexts/EventContext'
+import useBook from '../hooks/useBook'
 
 const title = css`
   font-weight: 600;
@@ -26,22 +23,14 @@ const description = css`
 
 const BooksSubmit: NextPage<any> = ({ userData }) => {
   const router = useRouter()
-  const [book, setBook] = useState()
   const { eventId } = useContext(EventContext)
   const circleRef = userData.event && userData.event[eventId]
+  const id = router.query.id as string
+  const { book } = useBook(id)
 
-  useEffect(() => {
-    if (!circleRef) return
-    const id = router.query.id as string
-    const query = db.collection('books').doc(id)
-    query.get().then(docRef => {
-      const data = docRef.data() as Book
-      if (circleRef!.id !== data.circle!.ref.id) {
-        router.push('/gishohaku1/mypage')
-      }
-      setBook({ id, ...data })
-    })
-  }, [userData, router.query.id])
+  if (!circleRef) {
+    return <p>サークル参加者向けページです。</p>
+  }
 
   if (!book) {
     return <Loader />
