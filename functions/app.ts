b57357e699ec289782bd25345bc67e8a6c7d4112
mottox2 <y.mottox2@gmail.com>
2019-12-05 21:29:08 +0900

    Disable app function in emulator

--- functions/app.ts
+++ functions/app.ts
@@ -1,10 +1,14 @@
 import * as functions from 'firebase-functions'
 import next from 'next'
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev, conf: { distDir: 'next' } })
-const handler = app.getRequestHandler()
+if (process.env.DISABLE_APP) {
+  const dev = process.env.NODE_ENV !== 'production'
+  const app = next({ dev, conf: { distDir: 'next' } })
+  const handler = app.getRequestHandler()
+}
 
-export default functions.https.onRequest((req, res) =>
-  app.prepare().then(() => handler(req, res))
-)
+export default functions.https.onRequest((req, res) => {
+  if (process.env.DISABLE_APP) {
+    app.prepare().then(() => handler(req, res))
+  }
+})
