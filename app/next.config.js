8abc0b050295f026b16b0da28387be75edb72323
mottox2 <y.mottox2@gmail.com>
2020-03-28 13:08:49 +0900

    refactor: reduce envs

--- app/next.config.js
+++ app/next.config.js
@@ -11,10 +11,7 @@ module.exports = withImages(
       pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
       env: {
         API_KEY: process.env.API_KEY,
-        AUTH_DOMAIN: process.env.AUTH_DOMAIN,
-        DATABASE_URL: process.env.DATABASE_URL,
         PROJECT_ID: process.env.PROJECT_ID,
-        STORAGE_BUCKET: process.env.STORAGE_BUCKET,
         SENTRY_DSN: process.env.SENTRY_DSN
       },
       distDir: '../dist/functions/next',
