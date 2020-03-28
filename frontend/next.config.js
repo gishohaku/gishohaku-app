a1d747307a8547c9eb144dd45da765b715b1489f
mottox2 <y.mottox2@gmail.com>
2020-03-28 20:12:06 +0900

    fix: fix config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,9 @@ const withMDX = require('@next/mdx')({
 const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
 
+const firebaseConfig = process.env.FIREBASE
+  ? { distDir: '../dist/functions/next' }
+  : {}
 // compose使う
 module.exports = withImages(
   withCSS(
@@ -14,33 +17,53 @@ module.exports = withImages(
         PROJECT_ID: process.env.PROJECT_ID,
         SENTRY_DSN: process.env.SENTRY_DSN,
       },
-      // distDir: './dist/functions/next',
-      // outDir: '../dist/public',
-      // exportPathMap: defaultPathMap => {
-      //   delete defaultPathMap['/[eventId]/books']
-      //   delete defaultPathMap['/[eventId]/books/[id]']
-      //   delete defaultPathMap['/[eventId]/circles']
-      //   delete defaultPathMap['/[eventId]/circles/[id]']
-      //   delete defaultPathMap['/[eventId]/mypage']
-      //   delete defaultPathMap['/[eventId]/mypage/circle']
-      //   delete defaultPathMap['/[eventId]/mypage/join']
-      //   console.log(defaultPathMap)
-      //   const pathMap = {
-      //     ...defaultPathMap,
-      //     '/gishohaku1/mypage': { page: '/[eventId]/mypage', query: { eventId: 'gishohaku1' } },
-      //     '/gishohaku2/mypage': { page: '/[eventId]/mypage', query: { eventId: 'gishohaku2' } },
-      //     '/gishohaku1/mypage/circle': { page: '/[eventId]/mypage/circle', query: { eventId: 'gishohaku1' } },
-      //     '/gishohaku2/mypage/circle': { page: '/[eventId]/mypage/circle', query: { eventId: 'gishohaku2' } },
-      //     '/gishohaku1/mypage/join': { page: '/[eventId]/mypage/join', query: { eventId: 'gishohaku1' } },
-      //     '/gishohaku2/mypage/join': { page: '/[eventId]/mypage/join', query: { eventId: 'gishohaku2' } },
-      //     '/gishohaku1/mypage/book_stars': { page: '/[eventId]/mypage/book_stars', query: { eventId: 'gishohaku1' } },
-      //     '/gishohaku2/mypage/book_stars': { page: '/[eventId]/mypage/book_stars', query: { eventId: 'gishohaku2' } }
-      //   }
+      ...firebaseConfig,
+      exportPathMap: defaultPathMap => {
+        delete defaultPathMap['/[eventId]/books']
+        delete defaultPathMap['/[eventId]/books/[id]']
+        delete defaultPathMap['/[eventId]/circles']
+        delete defaultPathMap['/[eventId]/circles/[id]']
+        delete defaultPathMap['/[eventId]/mypage']
+        delete defaultPathMap['/[eventId]/mypage/circle']
+        delete defaultPathMap['/[eventId]/mypage/join']
+        const pathMap = {
+          ...defaultPathMap,
+          '/gishohaku1/mypage': {
+            page: '/[eventId]/mypage',
+            query: { eventId: 'gishohaku1' },
+          },
+          '/gishohaku2/mypage': {
+            page: '/[eventId]/mypage',
+            query: { eventId: 'gishohaku2' },
+          },
+          '/gishohaku1/mypage/circle': {
+            page: '/[eventId]/mypage/circle',
+            query: { eventId: 'gishohaku1' },
+          },
+          '/gishohaku2/mypage/circle': {
+            page: '/[eventId]/mypage/circle',
+            query: { eventId: 'gishohaku2' },
+          },
+          '/gishohaku1/mypage/join': {
+            page: '/[eventId]/mypage/join',
+            query: { eventId: 'gishohaku1' },
+          },
+          '/gishohaku2/mypage/join': {
+            page: '/[eventId]/mypage/join',
+            query: { eventId: 'gishohaku2' },
+          },
+          '/gishohaku1/mypage/book_stars': {
+            page: '/[eventId]/mypage/book_stars',
+            query: { eventId: 'gishohaku1' },
+          },
+          '/gishohaku2/mypage/book_stars': {
+            page: '/[eventId]/mypage/book_stars',
+            query: { eventId: 'gishohaku2' },
+          },
+        }
 
-      //   console.log('== Result')
-      //   console.log(pathMap)
-      //   return pathMap
-      // }
+        return pathMap
+      },
     }),
   ),
 )
