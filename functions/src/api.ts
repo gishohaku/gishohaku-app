0a65eb7201b711f472bb40174de03de288771473
mottox2 <y.mottox2@gmail.com>
2021-07-03 22:28:30 +0900

    Add book endpoint

--- functions/src/api.ts
+++ functions/src/api.ts
@@ -47,3 +47,18 @@ export const books = onRequest(async (_req, res) => {
   res.set('Access-Control-Allow-Methods', 'GET, PURGE')
   res.status(200).send(JSON.stringify(books))
 })
+
+// gishohaku/og-imageで利用するためのエンドポイント
+export const book = onRequest(async (req, res) => {
+  const bookId = req.query.id as string
+  if (!bookId) {
+    return res.status(400).end()
+  }
+  const ref = await admin.firestore().collection('books').doc(bookId).get()
+  const data = ref.data()
+  res.set('Content-Type', 'application/json')
+  res.set('Cache-Control', 'public, max-age=60, s-maxage=600')
+  res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Methods', 'GET, PURGE')
+  res.status(200).send(JSON.stringify(data))
+})
