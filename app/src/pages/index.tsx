c497c86f337b3903a7ed6ff5e3cb2f2d8aa2ea0f
Akira Morikawa <ariakira@gmail.com>
2023-08-09 22:01:22 +0900

    スポンサーリンク追加

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -155,7 +155,7 @@ const Hero = () => (
         css={css`
           margin-top: 16px;
           margin-bottom: 16px;
-          .btn-orange {
+          .btn-blue {
             border: 2px solid #81b4c5;
             background-color: #81b4c5;
             color: #f2f7f9;
@@ -164,9 +164,21 @@ const Hero = () => (
               color: #81b4c5;
             }
           }
+          .btn-green {
+            border: 2px solid #81c5b4;
+            background-color: #81c5b4;
+            color: #f2f7f9;
+            &:hover {
+              background-color: #f2f9f7;
+              color: #81c5b4;
+            }
+          }
         `}>
-        <LinkButton href="/circle-entry" className="btn-orange">
-          サークル申込はこちら
+        <LinkButton href="/circle-entry" className="btn-blue">
+          サークル申込
+        </LinkButton>
+        <LinkButton href="https://docs.google.com/presentation/d/1HXrPaTPJnFhAdMEwRa1q2XNMneZjfO-WFNKhVzDtNyk/edit?usp=sharing" className="btn-green">
+          協賛要項
         </LinkButton>
       </div>
     </div>
