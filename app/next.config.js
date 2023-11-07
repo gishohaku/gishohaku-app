4d041ebab4de1ab1630dedd07fdc0a952a57ce39
Akira Morikawa <ariakira@gmail.com>
2023-11-07 11:03:11 +0900

    test

--- app/next.config.js
+++ app/next.config.js
@@ -30,38 +30,6 @@ module.exports = withImages(
           page: '/[eventId]/mypage',
           query: { eventId: 'gishohaku2' },
         },
-        '/gishohaku3/mypage': {
-          page: '/[eventId]/mypage',
-          query: { eventId: 'gishohaku3' },
-        },
-        '/gishohaku4/mypage': {
-          page: '/[eventId]/mypage',
-          query: { eventId: 'gishohaku4' },
-        },
-        '/gishohaku5/mypage': {
-          page: '/[eventId]/mypage',
-          query: { eventId: 'gishohaku5' },
-        },
-        '/gishohaku6/mypage': {
-          page: '/[eventId]/mypage',
-          query: { eventId: 'gishohaku6' },
-        },
-        '/gishohaku7/mypage': {
-          page: '/[eventId]/mypage',
-          query: { eventId: 'gishohaku7' },
-        },
-        '/gishohaku8/mypage': {
-          page: '/[eventId]/mypage',
-          query: { eventId: 'gishohaku8' },
-        },
-        '/gishohaku9/mypage': {
-          page: '/[eventId]/mypage',
-          query: { eventId: 'gishohaku9' },
-        },
-        '/gishohaku10/mypage': {
-          page: '/[eventId]/mypage',
-          query: { eventId: 'gishohaku10' },
-        },
         '/gishohaku1/mypage/circle': {
           page: '/[eventId]/mypage/circle',
           query: { eventId: 'gishohaku1' },
@@ -70,38 +38,6 @@ module.exports = withImages(
           page: '/[eventId]/mypage/circle',
           query: { eventId: 'gishohaku2' },
         },
-        '/gishohaku3/mypage/circle': {
-          page: '/[eventId]/mypage/circle',
-          query: { eventId: 'gishohaku3' },
-        },
-        '/gishohaku4/mypage/circle': {
-          page: '/[eventId]/mypage/circle',
-          query: { eventId: 'gishohaku4' },
-        },
-        '/gishohaku5/mypage/circle': {
-          page: '/[eventId]/mypage/circle',
-          query: { eventId: 'gishohaku5' },
-        },
-        '/gishohaku6/mypage/circle': {
-          page: '/[eventId]/mypage/circle',
-          query: { eventId: 'gishohaku6' },
-        },
-        '/gishohaku7/mypage/circle': {
-          page: '/[eventId]/mypage/circle',
-          query: { eventId: 'gishohaku7' },
-        },
-        '/gishohaku8/mypage/circle': {
-          page: '/[eventId]/mypage/circle',
-          query: { eventId: 'gishohaku8' },
-        },
-        '/gishohaku9/mypage/circle': {
-          page: '/[eventId]/mypage/circle',
-          query: { eventId: 'gishohaku9' },
-        },
-        '/gishohaku10/mypage/circle': {
-          page: '/[eventId]/mypage/circle',
-          query: { eventId: 'gishohaku10' },
-        },
         '/gishohaku1/mypage/join': {
           page: '/[eventId]/mypage/join',
           query: { eventId: 'gishohaku1' },
@@ -109,38 +45,6 @@ module.exports = withImages(
         '/gishohaku2/mypage/join': {
           page: '/[eventId]/mypage/join',
           query: { eventId: 'gishohaku2' },
-        },
-        '/gishohaku3/mypage/join': {
-          page: '/[eventId]/mypage/join',
-          query: { eventId: 'gishohaku3' },
-        },
-        '/gishohaku4/mypage/join': {
-          page: '/[eventId]/mypage/join',
-          query: { eventId: 'gishohaku4' },
-        },
-        '/gishohaku5/mypage/join': {
-          page: '/[eventId]/mypage/join',
-          query: { eventId: 'gishohaku5' },
-        },
-        '/gishohaku6/mypage/join': {
-          page: '/[eventId]/mypage/join',
-          query: { eventId: 'gishohaku6' },
-        },
-        '/gishohaku7/mypage/join': {
-          page: '/[eventId]/mypage/join',
-          query: { eventId: 'gishohaku7' },
-        },
-        '/gishohaku8/mypage/join': {
-          page: '/[eventId]/mypage/join',
-          query: { eventId: 'gishohaku8' },
-        },
-        '/gishohaku9/mypage/join': {
-          page: '/[eventId]/mypage/join',
-          query: { eventId: 'gishohaku9' },
-        },
-        '/gishohaku10/mypage/join': {
-          page: '/[eventId]/mypage/join',
-          query: { eventId: 'gishohaku10' },
         },
         '/gishohaku2/mypage/join': {
           page: '/[eventId]/mypage/join',
@@ -153,38 +57,6 @@ module.exports = withImages(
         '/gishohaku2/mypage/book_stars': {
           page: '/[eventId]/mypage/book_stars',
           query: { eventId: 'gishohaku2' },
-        },
-        '/gishohaku3/mypage/book_stars': {
-          page: '/[eventId]/mypage/book_stars',
-          query: { eventId: 'gishohaku3' },
-        },
-        '/gishohaku4/mypage/book_stars': {
-          page: '/[eventId]/mypage/book_stars',
-          query: { eventId: 'gishohaku4' },
-        },
-        '/gishohaku5/mypage/book_stars': {
-          page: '/[eventId]/mypage/book_stars',
-          query: { eventId: 'gishohaku5' },
-        },
-        '/gishohaku6/mypage/book_stars': {
-          page: '/[eventId]/mypage/book_stars',
-          query: { eventId: 'gishohaku6' },
-        },
-        '/gishohaku7/mypage/book_stars': {
-          page: '/[eventId]/mypage/book_stars',
-          query: { eventId: 'gishohaku7' },
-        },
-        '/gishohaku8/mypage/book_stars': {
-          page: '/[eventId]/mypage/book_stars',
-          query: { eventId: 'gishohaku8' },
-        },
-        '/gishohaku9/mypage/book_stars': {
-          page: '/[eventId]/mypage/book_stars',
-          query: { eventId: 'gishohaku9' },
-        },
-        '/gishohaku0/mypage/book_stars': {
-          page: '/[eventId]/mypage/book_stars',
-          query: { eventId: 'gishohaku10' },
         },
       }
 
