87d35d595fb91b3c6dc5f1b79b87a7cb23cb77c0
mottox2 <y.mottox2@gmail.com>
2023-12-24 15:22:09 +0900

    Remove book submission

--- functions/src/slack.ts
+++ functions/src/slack.ts
@@ -5,24 +5,6 @@ import { PubSub } from '@google-cloud/pubsub'
 
 const onRequest = functions.https.onRequest
 
-// TODO: slack-actions
-export const actions = onRequest(async (req, res) => {
-  const body = JSON.parse(req.body.payload)
-  const action = body.actions[0]
-
-  if (action.action_id === 'approve_submission') {
-    const bookId = ''
-    const ref = admin.firestore().collection('bookSubmissions').doc(bookId)
-    await ref.update({ isChecked: true })
-    await axios.post(body.response_url, {
-      text: `チェックしました`
-    })
-    return res.status(200).send()
-  }
-
-  res.status(200).send()
-})
-
 // slack-commands
 export const commands = onRequest(async (req, res) => {
   const body = req.body
