4c0c4e623e53db10805981d3f01c9ef6dd1ec59f
Akira Morikawa <ariakira@gmail.com>
2024-02-05 18:48:13 +0900

    技書博10サークル登録ボタン削除

--- app/src/pages/gishohaku10/index.tsx
+++ app/src/pages/gishohaku10/index.tsx
@@ -151,23 +151,6 @@ const Hero = () => (
         `}>
         @大田区産業プラザPiO 大展示ホール
       </div>
-      <div
-        css={css`
-          margin-top: 16px;
-          margin-bottom: 16px;
-          a {
-            border: 2px solid #81b4c5;
-            color: #81b4c5;
-            &:hover {
-              background-color: #81b4c5;
-              color: white;
-            }
-          }
-        `}>
-        <LinkButton href="/gishohaku10/circle-entry">
-          サークル参加を申し込む
-        </LinkButton>
-      </div>
     </div>
   </div>
   </div>
