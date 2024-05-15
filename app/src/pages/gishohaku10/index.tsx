7361cb420392372c1e03a4c4de473ae9c60cf693
Akira Morikawa <ariakira@gmail.com>
2024-05-15 18:51:28 +0900

    fix award

--- app/src/pages/gishohaku10/index.tsx
+++ app/src/pages/gishohaku10/index.tsx
@@ -173,6 +173,96 @@ const Hero = () => (
   </div>
 )
 
+const Award: React.SFC<{
+  title: string
+  book: string
+  circle: string
+  image: string
+  bookref: string
+  circleref: string
+}> = ({ title, book, circle, image, bookref, circleref }) => {
+  return (
+    <div
+      css={css`
+        display: flex-item;
+        width: 300px;
+        text-align: center;
+        margin: 8px;
+      `}>
+      <div
+        css={css`
+          width: 300px;
+          margin: 0 auto;
+          display: -webkit-flex;
+          display: flex;
+          text-align: center;
+          -webkit-align-items: center;
+          align-items: center;
+          -webkit-justify-content: center;
+          justify-content: center;
+        `}>
+        <a
+          href={bookref || 'javascript:void(0);'}
+          css={css`
+            padding: 12px;
+            text-align: center;
+            display: block;
+          `}
+          rel="noopener">
+          <img
+            css={css`
+              max-width: 200px;
+              max-height: 240px;
+              margin: 0 auto;
+            `}
+            src={image}
+          />
+        </a>
+      </div>
+      <div
+        css={css`
+          font-size: 12px;
+          margin-top: 8px;
+          margin-bottom: 4px !important;
+          line-height: 1.4;
+          opacity: 0.8;
+        `}>{title}</div>
+      <div
+        css={css`
+          font-size: 15px;
+          font-weight: bold;
+          line-height: 1.5;
+          margin-bottom: 4px;
+        `}>
+        <a
+          css={css`
+            text-decoration: none;
+            color: #1d272d;
+          `}
+          href={bookref || 'javascript:void(0);'}
+          rel="noopener">
+          {book}
+        </a>
+      </div>
+      <div
+        css={css`
+          font-size: 15px;
+          line-height: 1.5;
+        `}>
+        <a
+          css={css`
+            text-decoration: none;
+            color: #1d272d;
+          `}
+          href={circleref || 'javascript:void(0);'}
+          rel="noopener">
+          {circle}
+        </a>
+      </div>
+    </div>
+  )
+}
+
 const Sponsor: React.SFC<{
   image: string
   name: string
