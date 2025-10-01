a73fa1f5800843b71ae8851a69eef0fc1ebf5566
Akira Morikawa <ariakira@gmail.com>
2025-10-01 19:55:23 +0900

    add gishohaku12 circles

---
+++ scripts/20250929-createInvitation.ts
@@ -0,0 +1,32 @@
+/**
+ * command-line:
+ * ts-node scripts/20240423-createInvitation.ts
+ */
+import admin from 'firebase-admin'
+
+admin.initializeApp({
+  projectId: process.env.PROJECT_ID,
+  databaseURL: process.env.DATABASE_URL,
+  storageBucket: process.env.STORAGE_BUCKET
+});
+
+(async () => {
+  const db = admin.firestore()
+  const query = db.collection('circles').where("eventId", "==", "gishohaku12")
+  const snapshot = await query.get()
+  snapshot.docs.forEach(async doc => {
+    const { name, booth } = doc.data()
+    // console.log(doc.id, name)
+    const result = await doc.ref.collection('circleInvitations').add({
+      eventId: "gishohaku12"
+    })
+    const token = result.id
+    console.log([
+      doc.id,
+      booth,
+      name,
+      token,
+      `https://gishohaku.dev/gishohaku12/mypage/join?circleId=${doc.id}&token=${token}`
+    ].join(", "))
+  })
+})()
